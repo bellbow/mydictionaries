fa01de582c4a13e2f5b164913c1203192dc9b43f
Bella Ball <mohawkball@gmail.com>
2023-07-20 17:17:17 -0500

    Finished file 7

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -1,6 +1,6 @@
 """
 Process the JSON file named school_data.json. Display only those schools 
-that are part of the ACC, Big 12, Big Ten, Pac-12 and SEC divisons. This
+that are part of the AAC, Big 12, Big Ten, and SEC divisons. This
 information can be found in the ValueLabels file.
 
 Copy that info here:
@@ -17,3 +17,26 @@ Display report for all universities that have a total price for in-state student
 
 
 """
+
+import json
+
+infile = open('school_data.json','r')
+schools = json.load(infile)
+confrence_schools = [372,108,107,130]
+
+print('AAC, Big 12, Big Ten, or SEC:')
+for school in schools:
+    if school['NCAA']['NAIA conference number football (IC2020)'] in confrence_schools:
+        print(school['instnm'])   
+
+print('\nWomen grad rate over 50:')
+for school in schools:
+    if type(school['Graduation rate  women (DRVGR2020)']) == int:
+        if school['Graduation rate  women (DRVGR2020)'] > 50:
+            print(school['instnm'])
+
+print('\nIn-state price over 50000:')
+for school in schools:
+    if type(school['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)']) == int:
+        if school['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)'] > 50000:
+            print(school['instnm'])
