9b06ad96cd36486aae3c3546b3063fa4d12e2d09
Bella Ball <mohawkball@gmail.com>
2023-07-20 19:14:55 -0500

    edited infile location

--- WordFrequency.py
+++ WordFrequency.py
@@ -5,7 +5,7 @@ The program should create a dictionary in which the keys are the individual word
 For example, if the word “the” appears 128 times, the dictionary would contain an element with 'the' as the key and 128 as the value. 
 The program should display the frequency of each word.
 '''
-infile = open(r'C:\Users\mohaw\Downloads\sometext-1.txt','r')
+infile = open(r'sometext-1.txt','r')
 text = infile.read()
 
 punc = [',','.']
